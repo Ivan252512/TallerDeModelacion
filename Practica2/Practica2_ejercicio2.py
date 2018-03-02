92890f4206d716d70203a02770f49b915bebc667
Ivan252512 <ivanpineda@ciencias.unam.mx>
2018-03-02 17:11:53 -0600

    empezamos

---
+++ Practica2/Practica2_ejercicio2.py
@@ -0,0 +1,2 @@
+"""Aproximar la segunda derivada de f(x)=2**(sin(x**2+1))*cos*sqrt(|sin(x-1)|)
+en x=sqrt(2), usar h=0.001 y diferencias centradas"""
---
+++ Practica2/Practica2_ejercicio3.py
@@ -0,0 +1,2 @@
+"""Aproximar la segunda parcial con respecto a x para
+f(x,y,x)=(e**(x**2+sqrt(|x-y+5z|))*sin(x**2+1))/(x**2+2) con (sqrt(2),pi,0)"""
