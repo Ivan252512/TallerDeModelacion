dd7a2f83292d264d28744495f21495962b1771c6
Ivan252512 <ivanpineda@ciencias.unam.mx>
2018-04-17 22:30:02 -0500

    Práctica 7

--- Practica7/practica7.py
+++ Practica7/practica7.py
@@ -13,7 +13,7 @@ num=funciones.rk4_1d(f,a,b,stp,[0,2])
 
 plt.plot(num[0],num[1],label='f(t,y)=y²')
 plt.title("Práctica 7")
-plt.ylabel('y(t)')
+plt.ylabel("y'(t)")
 plt.xlabel('t')
 plt.legend(loc=2)
 plt.savefig( 'practica7.png', fmt='PNG', dpi=100 )
