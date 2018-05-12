f1f1b7a3d93e951974b79083b318a0c1c4ae0100
Ivan252512 <ivanpineda@ciencias.unam.mx>
2018-05-11 22:52:40 -0500

    Práctica 9

--- Practica9/practica9.py
+++ Practica9/practica9.py
@@ -27,23 +27,23 @@ a=1; #Inicio del dominio.
 b=2; #Fin del dominio.
 stp=0.01; #Pasos entre cada punto.
 m0=-2
+w=1
+beta=0
 
-while
-num=rk.rk45ind(g,e,k,l,a,b,stp,[0,m0,0,1])
-#y(t)=y1(t)+beta-(y1(b)/z(b))*z(t)  ,z(b)!=0
+plt.figure(figsize=(15,7))
 
-w=abs(num[1][len(num[1])-1]-(-2))
-m0=m0-((num[1][len(num[1])-1]-(-2))/num[3][len(num[3])-1])
+while(w>1e-7):
+	num=rk.rk45ind(g,e,k,l,a,b,stp,[0,m0,0,1])
+	#y(t)=y1(t)+beta-(y1(b)/z(b))*z(t)  ,z(b)!=0
+	ydet=(np.array(num[1])+beta-((num[1][len(num[1])-1])/
+		  num[3][len(num[3])-1]) * np.array(num[3]))
+	plt.plot(num[0],ydet,label="w = " + str(w) + ", m0 = " + str(m0))
+	w=abs((num[3][len(num[3])-1])-(-2))
+	m0=m0-((num[3][len(num[3])-1]-(-2))/num[1][len(num[1])-1])
 
-
-ydet=(np.array(num[1])+beta-((num[1][len(num[1])-1])/num[3][len(num[3])-1]) *
-     np.array(num[3]))
-
-plt.figure(figsize=(20,10))
-plt.plot(num[0],ydet,label=" y'' = -2yy' + ty' + y + t ")
-plt.title("Práctica 9 ")
+plt.title("Práctica 9 " +"\n" + "y'' = -2yy' + ty' + y + t")
 plt.ylabel("y(t)")
 plt.xlabel('t')
-plt.legend(loc=2)
+plt.legend(loc=1)
 plt.savefig( 'practica9.png', fmt='PNG', dpi=400 )
 plt.show()
