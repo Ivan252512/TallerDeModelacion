ec2634b6e879c3b22917aef2f10ffe5143e4719e
Ivan252512 <ivanpineda@ciencias.unam.mx>
2018-05-11 20:03:47 -0500

    Práctica 9

--- Practica9/practica9.py
+++ Practica9/practica9.py
@@ -15,7 +15,7 @@ def g(t, y, yp, z, zp ):
 	return yp
 
 def e(t, y, yp, z, zp ):
-	return -2*(y*yp-t*yp+y+t)
+	return -2*(y*yp+t*yp+y+t)
 
 def k(t, y, yp, z, zp ):
 	return zp
@@ -24,47 +24,26 @@ def l(t, y, yp, z, zp ):
 	return z*(-2*yp-2)+zp*(-2*y-2*t)
 
 a=1; #Inicio del dominio.
-b=1.5; #Fin del dominio.
-stp=0.01; #Pasos entre cada punto.
-m0=-2
-
-num=rk.rk45ind(g,e,k,l,a,b,stp,[0,m0,0,1])
-#y(t)=y1(t)+beta-(y1(b)/z(b))*z(t)  ,z(b)!=0
-beta=0
-
-ydet=(np.array(num[1])+beta-((num[1][len(num[1])-1])/num[3][len(num[3])-1]) *
-     np.array(num[3]))
-
-
-plt.figure(figsize=(20,10))
-plt.plot(num[0],ydet,label=" y'' = -2yy' + ty' + y + t ")
-plt.title("Práctica 9 1<=t<=1.5 ")
-plt.ylabel("y(t)")
-plt.xlabel('t')
-plt.legend(loc=2)
-plt.savefig( 'practica9_1.png', fmt='PNG', dpi=400 )
-plt.show()
-
-
-a=1.5; #Inicio del dominio.
 b=2; #Fin del dominio.
 stp=0.01; #Pasos entre cada punto.
 m0=-2
 
+while
 num=rk.rk45ind(g,e,k,l,a,b,stp,[0,m0,0,1])
 #y(t)=y1(t)+beta-(y1(b)/z(b))*z(t)  ,z(b)!=0
-beta=0
+
+w=abs(num[1][len(num[1])-1]-(-2))
+m0=m0-((num[1][len(num[1])-1]-(-2))/num[3][len(num[3])-1])
+
 
 ydet=(np.array(num[1])+beta-((num[1][len(num[1])-1])/num[3][len(num[3])-1]) *
      np.array(num[3]))
 
-
 plt.figure(figsize=(20,10))
 plt.plot(num[0],ydet,label=" y'' = -2yy' + ty' + y + t ")
-plt.title("Práctica 9 1.5<=t<=2 ")
+plt.title("Práctica 9 ")
 plt.ylabel("y(t)")
 plt.xlabel('t')
 plt.legend(loc=2)
-plt.savefig( 'practica9_2.png', fmt='PNG', dpi=400 )
+plt.savefig( 'practica9.png', fmt='PNG', dpi=400 )
 plt.show()
-
