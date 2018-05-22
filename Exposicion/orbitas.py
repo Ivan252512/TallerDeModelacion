8309d97a14a16816b362b2128f9d02e4894d917d
Ivan252512 <ivanpineda@ciencias.unam.mx>
2018-05-21 20:51:08 -0500

    AG

--- Exposicion/orbitas.py
+++ Exposicion/orbitas.py
@@ -1,8 +1,8 @@
 from vpython import *
 import math
-import funciones
+import numpy as np
 
-G= 39.478417
+G= 39.4784176 # AU⁻³ YR⁻² SM-¹
 pi = 3.141592654
 dospi=2*pi
 pimedios=pi/2
@@ -27,7 +27,7 @@ wmarte=vmarte/rmarte   # Velocidad angular de Marte
 
 """Jupiter"""
 rjup=5.2
-mjup=954.79e-6
+mjup=954.79e-6 #-4
 vjup=sqrt(G/rjup)
 wjup=vjup/rjup
 
@@ -50,76 +50,73 @@ vnep=sqrt(G/rnep)
 wnep=vnep/rnep
 
 """Nave"""
-vship=sqrt((2*G*rmarte)/rship/(rship+rmarte))  #Velocidad Inicial nave
+condicionesIniciales=[-20.,         -16.11339198, 709.87292278]
+vship=vector(condicionesIniciales[0], condicionesIniciales[1],0) #Velocidad Inicial nave
+ANG=pi/2
 
-
-ANG=pi
-print('Posición angular inicial, ', ANG*180/pi)
 sa = sin(ANG)
 ca = cos(ANG)
 sca = sin(pimedios+ANG)
 cca = cos(pimedios+ANG)
-Rmin=rmarte*(mmarte)**0.4
 
 Sun = sphere(pos=vector(0,0,0),
-             radius=0.1, color=color.yellow,make_trail=False, interval=10)
+             radius=0.5, color=color.yellow,make_trail=False, interval=10)
 Sun.mass = 1
 Sun.v = vector(0, 0, 0)
 
 Earth = sphere(pos=vector(1,0,0),
-               radius=0.01, color=color.blue,make_trail=False, interval=10)
+               radius=0.1, color=color.blue,make_trail=False, interval=10)
 Earth.mass = 3.004e-6
 Earth.v = vector(0, 6.283, 0)
 
 Mercury = sphere(pos=vector(rmerc*ca,rmerc*sa,0),
-                 radius=0.005, color=color.red,make_trail=False, interval=10)
+                 radius=0.05, color=color.red,make_trail=False, interval=10)
 Mercury.mass = mmerc
 Mercury.v = vector(vmerc*cca, vmerc*sca, 0)
 
 Venus = sphere(pos=vector(rvenus*ca,rvenus*sa,0),
-               radius=0.01, color=color.red,make_trail=False, interval=10)
+               radius=0.1, color=color.red,make_trail=False, interval=10)
 Venus.mass = mvenus
 Venus.v = vector(vvenus*cca, vvenus*sca, 0)
 
 Mars = sphere(pos=vector(rmarte*ca,rmarte*sa,0),
-              radius=0.01, color=color.red,make_trail=False, interval=10)
+              radius=0.1, color=color.red,make_trail=False, interval=10)
 Mars.mass = mmarte
 Mars.v = vector(vmarte*cca, vmarte*sca, 0)
 
 Jupyter = sphere(pos=vector(rjup*ca,rjup*sa,0),
-                 radius=0.07, color=color.green,make_trail=False, interval=10)
+                 radius=0.3, color=color.green,make_trail=False, interval=10)
 Jupyter.mass = mjup
 Jupyter.v = vector(vjup*cca, vjup*sca, 0)
 
 Saturn = sphere(pos=vector(rsat*ca,rsat*sa,0),
-                radius=0.05, color=color.green,make_trail=False, interval=10)
+                radius=0.25, color=color.green,make_trail=False, interval=10)
 Saturn.mass = msat
 Saturn.v = vector(vsat*cca, vsat*sca, 0)
 
 Uranus = sphere(pos=vector(rur*ca,rur*sa,0),
-                radius=0.04, color=color.green,make_trail=False, interval=10)
+                radius=0.2, color=color.green,make_trail=False, interval=10)
 Uranus.mass = mur
 Uranus.v = vector(vur*cca, vur*sca, 0)
 
 Neptune = sphere(pos=vector(rnep*ca,rnep*sa,0),
-                 radius=0.04, color=color.green,make_trail=False, interval=10)
+                 radius=0.2, color=color.green,make_trail=False, interval=10)
 Neptune.mass = mnep
 Neptune.v = vector(vnep*cca, vnep*sca, 0)
 
 Ship = sphere(pos=vector(rship, 0, 0),
-              radius=0.0001, color=color.orange,make_trail=True, interval=10)
+              radius=0.00001, color=color.orange,make_trail=True, interval=10)
 Ship.mass = 3.214e-28
-Ship.v = vector(0, vship, 0)
+Ship.v = vship
 
 dt = 2.73785078e-4
-print('Radio de influencia del planeta, ', Rmin)
-flag=0
-
-
+tiempo=0
 while True:
-    rate(2000)
+    rate(500)
+    if tiempo==int(condicionesIniciales[2]):
+        body.pos=Earth.pos
     for body in [Mercury,Venus,Earth,Mars,Jupyter,Saturn,Uranus,Neptune,Ship]:
-        if body == Ship:
+        if body == Ship and tiempo>=int(condicionesIniciales[2]):
             DS=body.pos-Sun.pos
             DM=body.pos-Mercury.pos
             DV=body.pos-Venus.pos
@@ -140,8 +137,9 @@ while True:
                      Neptune.mass*DN/mag(DN)**3)
             body.pos += body.v*dt + 0.5*as1*dt*dt
             body.v += as1*dt
-        else:
+        elif body!=Ship:
             distance = body.pos - Sun.pos
             a1 = -G*Sun.mass*distance/mag(distance)**3
             body.pos += body.v*dt + 0.5*a1*dt*dt
             body.v += a1*dt
+    tiempo+=1
