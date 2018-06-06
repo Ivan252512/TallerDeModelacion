727be1285fa65c2f0430bdde6cc3612411fef2e1
Ivan252512 <ivanpineda@ciencias.unam.mx>
2018-06-06 11:11:30 -0500

    Final

--- practicaFinal/funciones.py
+++ practicaFinal/funciones.py
@@ -74,6 +74,34 @@ def rk51D(f,a,b,h,VI):
     return x,y
 
 
+def rk43ind(f,g,e,a,b,h,VI):
+    N=int((b-a)/h)
+    x = np.arange(a, b+h, h)
+    y = np.zeros((N+1,))
+    z = np.zeros((N+1,))
+    x[0], y[0], z[0] = VI
+    for i in range(1,N+1):
+        k1x = h*f(x[i-1], y[i-1], z[i-1])
+        k1y = h*g(x[i-1], y[i-1], z[i-1])
+        k1z = h*e(x[i-1], y[i-1], z[i-1])
+
+        k2x = h*f(x[i-1]+ k1x/2.0, y[i-1] + k1y/2.0, z[i-1] + k1z/2.0)
+        k2y = h*g(x[i-1]+ k1x/2.0, y[i-1] + k1y/2.0, z[i-1] + k1z/2.0)
+        k2z = h*e(x[i-1]+ k1x/2.0, y[i-1] + k1y/2.0, z[i-1] + k1z/2.0)
+
+        k3x = h*f(x[i-1]+ k2x/2.0, y[i-1] + k2y/2.0, z[i-1] + k2z/2.0)
+        k3y = h*g(x[i-1]+ k2x/2.0, y[i-1] + k2y/2.0, z[i-1] + k2z/2.0)
+        k3z = h*e(x[i-1]+ k2x/2.0, y[i-1] + k2y/2.0, z[i-1] + k2z/2.0)
+
+        k4x = h*f(x[i-1]+ k3x, y[i-1] + k3y, z[i-1] + k3z)
+        k4y = h*g(x[i-1]+ k3x, y[i-1] + k3y, z[i-1] + k3z)
+        k4z = h*e(x[i-1]+ k3x, y[i-1] + k3y, z[i-1] + k3z)
+
+        x[i] = x[i-1] + k1x/6.0 + k2x/3.0 + k3x/3.0 + k4x/6.0
+        y[i] = y[i-1] + k1y/6.0 + k2y/3.0 + k3y/3.0 + k4y/6.0
+        z[i] = z[i-1] + k1z/6.0 + k2z/3.0 + k3z/3.0 + k4z/6.0
+    return x,y,z
+
 def rk43d(g,e,a,b,h,VI):
     N=int((b-a)/h)
     x = np.arange(a, b+h, h)
--- practicaFinal/main.py
+++ practicaFinal/main.py
@@ -21,10 +21,14 @@ while True:
     except ValueError:
         print ('Carácter no válido, debe ser un número entero.')
 
+print('¿Alguna variable es independiente de todas las demás? 0 no y 1 sí')
+print('Si no está seguro ingrese 1, p. ej. es cuando el tiempo es independiente')
+dep = int(input('Ingrese 1 o 0: '))
+
 #Asignación de variables
 print('Introduce las variables que vas a utilizar (e.g x ó t ó z),'
       +' toma en cuenta que el número de variables es el número de'
-      +' funciones más 1.')
+      +' funciones más 1 (si alguna variable no depende de las demás).')
 print('La variable de más es f(variable independiente), si te sobran variables'
       + ' pon culquier letra, p. ej y, es necesario para realizar el método.')
 if numFunc==1:
@@ -37,8 +41,9 @@ if numFunc==2:
 if numFunc==3:
     var0=input('Introduce tu variable 1:  ')
     var1=input('Introduce tu variable 2:  ')
-    var3=input('Introduce tu variable 3:  ')
-    var4=input('Introduce tu variable 4:  ')
+    var2=input('Introduce tu variable 3:  ')
+    if dep==1:
+        var3=input('Introduce tu variable 4:  ')
 if numFunc==4:
     var0=input('Introduce tu variable 1:  ')
     var1=input('Introduce tu variable 2:  ')
@@ -58,15 +63,26 @@ if numFunc==2:
     gs = input('Introduce tu función 2:  ')
     exec('g = lambda ' + var0 + ', ' + var1 + ', ' + var2 + ' : ' + gs)
 if numFunc==3:
-    fs = input('Introduce tu función 1:  ')
-    exec('f = lambda ' + var0 + ', ' + var1 + ', ' + var2 + ', ' + var3 +
-         ' : ' + fs)
-    gs = input('Introduce tu función 2:  ')
-    exec('g = lambda ' + var0 + ', ' + var1 + ', ' + var2 + ', ' + var3 +
-         ' : ' + gs)
-    es = input('Introduce tu función 3:  ')
-    exec('e = lambda ' + var0 + ', ' + var1 + ', ' + var2 + ', ' + var3 +
-         ' : ' + es)
+    if dep==1:
+        fs = input('Introduce tu función 1:  ')
+        exec('f = lambda ' + var0 + ', ' + var1 + ', ' + var2 + ', ' + var3 +
+             ' : ' + fs)
+        gs = input('Introduce tu función 2:  ')
+        exec('g = lambda ' + var0 + ', ' + var1 + ', ' + var2 + ', ' + var3 +
+             ' : ' + gs)
+        es = input('Introduce tu función 3:  ')
+        exec('e = lambda ' + var0 + ', ' + var1 + ', ' + var2 + ', ' + var3 +
+             ' : ' + es)
+    if dep==0:
+        fs = input('Introduce tu función 1:  ')
+        exec('f = lambda ' + var0 + ', ' + var1 + ', ' + var2 +
+             ' : ' + fs)
+        gs = input('Introduce tu función 2:  ')
+        exec('g = lambda ' + var0 + ', ' + var1 + ', ' + var2 +
+             ' : ' + gs)
+        es = input('Introduce tu función 3:  ')
+        exec('e = lambda ' + var0 + ', ' + var1 + ', ' + var2 +
+             ' : ' + es)
 if numFunc==4:
     fs = input('Introduce tu función 1:  ')
     exec('f = lambda ' + var0 + ', ' + var1 + ', ' + var2 + ', ' + var3 +
@@ -92,6 +108,8 @@ print('El valor inicial para tu variable 1 es a = ' + str(a) )
 for i in range(numFunc):
     VI.append(float(input('Introduce el valor inicial para tu variable ' +
                           str(i+2) + ':  ')))
+    if dep==0 and i==numFunc-2:
+        break
 
 #Escoger el método para resolver
 if numFunc==1:
@@ -125,13 +143,19 @@ if numFunc==1:
 if numFunc==2:
     print('El método que se utilizará es Runge-Kutta 4° orden')
     x, y, z = funciones.rk43d(f, g, a, b, h, VI)
-    grafica.graf3Var(x, y, z, )
+    grafica.graf3Var(x, y, z, 'Runge-Kutta 4° orden', 'f: ' + fs + '\n' + 'g :'
+                     + gs, var0, var1, var2)
 
 if numFunc==3:
     print('El método que se utilizará es Runge-Kutta 4° orden')
-    print('Como no se pueden graficar 4 variables, solo se regresan los' +
-          'valores.')
-    print(funciones.rk44d(f, g, e, a, b, h, VI))
+    if dep==0:
+        x, y, z = funciones.rk43ind(f, g, e, a, b, h, VI)
+        grafica.graf3Var(x, y, z, 'Runge-Kutta 4° orden', 'f: ' + fs + '\n'
+                         + 'g :' + gs + '\n' + 'e :' + es, var0, var1, var2)
+    if dep==1:
+        print('Como no se pueden graficar 4 variables, solo se regresan los' +
+              'valores.')
+        print(funciones.rk44d(f, g, e, a, b, h, VI))
 
 if numFunc==4:
     print('El método que se utilizará es Runge-Kutta 4° orden')
@@ -144,24 +168,24 @@ if numFunc==4:
         print(funciones.rk45d(f, g, e, l, a, b, h, VI))
     if disp=='1':
         EDPNL = input('Introduzca su ecuación no lineal:  ')
+        beta = float(input('Introduzca el valor de beta:  '))
         w = float(input('Introduzca el valor inicial de w:  '))
         lim = float(input('Introduzca el valor más pequeño que w puede tomar:'))
         while(w>lim):
             t, y, yp, z, zp = funciones.rk45d(f, g, e, l, a, b, h, VI)
             #y(t)=y1(t)+beta-(y1(b)/z(b))*z(t)  ,z(b)!=0
-            ydet=(np.array(y)+((b-((y[len(y)-1])))/
+            ydet=(np.array(y)+((beta-((y[len(y)-1])))/
             	  z[len(z)-1]) * np.array(z))
-            plt.plot(t,ydet, label="w = " + str(w) + ", m = " + str(VI[3]))
+            plt.plot(t,ydet, label="w = " + str(w) + ", m = " + str(VI[2]))
             plt.legend(loc=1)
             plt.title("Método de disparo: " + EDPNL)
             plt.ylabel("F(" + str(var0) + ")")
 
             zb=z[len(z)-1]
             yb=y[len(y)-1]
-            dif=yb-b
+            dif=yb-beta
             w=abs(dif)
-            VI[3]=VI[3]-(dif/zb)
-            break
+            VI[2]=VI[2]-(dif/zb)
         plt.xlabel('t')
         plt.savefig("Método de disparo: " + EDPNL + '.png', fmt='PNG', dpi=400 )
         plt.show()
