e85f7e3b8b7aafe50098dc69743a4506a48e5eeb
Iván Pineda <ivan@ivan.ivan.com>
2018-07-18 02:14:49 -0500

    rk

--- SistemaSolar/optimizado/funcion.py
+++ SistemaSolar/optimizado/funcion.py
@@ -2,9 +2,130 @@
 
 #vpython es una librería para visualización gráfica.
 from vpython import *
+import rungeKutta as rk
+
+#Constantes y condiciones iniciales.
+G= 39.4784176 # AU³ YR⁻² SM-¹
+UAkm = 1.496e+8 #km
+Msol = 1.989e+30 #kg
+pi = 3.141592654
+dospi=2*pi
+pimedios=pi/2
+"""Mercurio"""
+rmerc=0.38          #radio
+mmerc=0.166e-6      #masa
+vmerc=sqrt(G/rmerc) #velocidad tangencial
+wmerc=vmerc/rmerc   #velocidad angular
+
+"""Venus"""
+rvenus=0.723
+mvenus=2.44e-6
+vvenus=sqrt(G/rvenus)
+wvenus=vvenus/rvenus
+
+"""Earth"""
+rearth = 1
+mearth = 3.004e-6
+vearth = sqrt(G/rearth)
+wearth = vearth/rearth
+
+"""Luna"""
+rluna = (UAkm + 384400)/UAkm
+mluna = (7.349e+22)/Msol
+vluna = sqrt(G/rluna)
+wluna = vluna/rluna
+
+"""Marte"""
+rmarte=1.53 #Distancia media al Sol del planeta de destino UA
+mmarte=3.214e-7 #Masa del planeta de destino MS
+vmarte=sqrt(G/rmarte)  #Velocidad de Marte
+wmarte=vmarte/rmarte   # Velocidad angular de Marte
+
+"""Jupiter"""
+rjup=5.2
+mjup=954.79e-6 #-4
+vjup=sqrt(G/rjup)
+wjup=vjup/rjup
+
+"""Saturno"""
+rsat=9.57
+msat=285.88e-6
+vsat=sqrt(G/rsat)
+wsat=vsat/rsat
+
+"""Urano"""
+rur=19.3
+mur=43.66e-6
+vur=sqrt(G/rur)
+wur=vur/rur
+
+"""Neptuno"""
+rnep=30.2
+mnep=51.51e-6
+vnep=sqrt(G/rnep)
+wnep=vnep/rnep
+
+#Ángulos para trabajar en coordenadas polares
+ANG=pi/2
+sa = sin(ANG)
+ca = cos(ANG)
+sca = sin(pimedios+ANG)
+cca = cos(pimedios+ANG)
+
+#Generación de objetos a representar con su posición, masa y velocidad.
+Sun = sphere(pos=vector(0,0,0),
+             radius=0.5, color=color.yellow,make_trail=False, interval=10)
+Sun.mass = 1
+Sun.v = vector(0, 0, 0)
+
+Earth = sphere(pos=vector(rearth*ca,rearth*sa,0),
+               radius=0.1, color=color.blue,make_trail=False, interval=10)
+Earth.mass = mearth
+Earth.v = vector(vearth*cca, vearth*sca, 0)
+
+Luna = sphere(pos=vector(rluna*ca,rluna*sa,0),
+               radius=0.1, color=color.blue,make_trail=True, interval=10)
+Luna.mass = mluna
+Luna.v = vector(vluna*cca, vluna*sca, 0)
+
+Mercury = sphere(pos=vector(rmerc*ca,rmerc*sa,0),
+                 radius=0.05, color=color.orange,make_trail=False, interval=10)
+Mercury.mass = mmerc
+Mercury.v = vector(vmerc*cca, vmerc*sca, 0)
+
+Venus = sphere(pos=vector(rvenus*ca,rvenus*sa,0),
+               radius=0.1, color=color.yellow,make_trail=False, interval=10)
+Venus.mass = mvenus
+Venus.v = vector(vvenus*cca, vvenus*sca, 0)
+
+Mars = sphere(pos=vector(rmarte*ca,rmarte*sa,0),
+              radius=0.1, color=color.red,make_trail=False, interval=10)
+Mars.mass = mmarte
+Mars.v = vector(vmarte*cca, vmarte*sca, 0)
+
+Jupyter = sphere(pos=vector(rjup*ca,rjup*sa,0),
+                 radius=0.3, color=color.orange,make_trail=False, interval=10)
+Jupyter.mass = mjup
+Jupyter.v = vector(vjup*cca, vjup*sca, 0)
+
+Saturn = sphere(pos=vector(rsat*ca,rsat*sa,0),
+                radius=0.25, color=color.cyan,make_trail=False, interval=10)
+Saturn.mass = msat
+Saturn.v = vector(vsat*cca, vsat*sca, 0)
+
+Uranus = sphere(pos=vector(rur*ca,rur*sa,0),
+                radius=0.2, color=color.blue,make_trail=False, interval=10)
+Uranus.mass = mur
+Uranus.v = vector(vur*cca, vur*sca, 0)
+
+Neptune = sphere(pos=vector(rnep*ca,rnep*sa,0),
+                 radius=0.2, color=color.blue,make_trail=False, interval=10)
+Neptune.mass = mnep
+Neptune.v = vector(vnep*cca, vnep*sca, 0)
 
 """Es la función principal, descripción en el archivo .pdf adjunto"""
 def f(vx,vy,inicio):
+    global Sun, Mercury,Venus,Earth,Luna,Mars,Jupyter,Saturn,Uranus,Neptune
     #Condición de penalización para que la rapidez de la nave no supere el
     #límite impuesto.
     if(sqrt(vx**2+vy**2)>7.5):
@@ -17,116 +138,10 @@ def f(vx,vy,inicio):
     scene.title = "Trayectorias óptimas en el Sistema Solar"
     scene.range = 36
 
-    #Constantes y condiciones iniciales.
-    G= 39.4784176 # AU³ YR⁻² SM-¹
-    pi = 3.141592654
-    dospi=2*pi
-    pimedios=pi/2
-    rship=1.01
-    """Mercurio"""
-    rmerc=0.38          #radio
-    mmerc=0.166e-6      #masa
-    vmerc=sqrt(G/rmerc) #velocidad tangencial
-    wmerc=vmerc/rmerc   #velocidad angular
-
-    """Venus"""
-    rvenus=0.723
-    mvenus=2.44e-6
-    vvenus=sqrt(G/rvenus)
-    wvenus=vvenus/rvenus
-
-    """Marte"""
-    rmarte=1.53 #Distancia media al Sol del planeta de destino UA
-    mmarte=3.214e-7 #Masa del planeta de destino MS
-    vmarte=sqrt(G/rmarte)  #Velocidad de Marte
-    wmarte=vmarte/rmarte   # Velocidad angular de Marte
-
-    """Jupiter"""
-    rjup=5.2
-    mjup=954.79e-6 #-4
-    vjup=sqrt(G/rjup)
-    wjup=vjup/rjup
-
-    """Saturno"""
-    rsat=9.57
-    msat=285.88e-6
-    vsat=sqrt(G/rsat)
-    wsat=vsat/rsat
-
-    """Urano"""
-    rur=19.3
-    mur=43.66e-6
-    vur=sqrt(G/rur)
-    wur=vur/rur
-
-    """Neptuno"""
-    rnep=30.2
-    mnep=51.51e-6
-    vnep=sqrt(G/rnep)
-    wnep=vnep/rnep
-
-    """Nave"""
-    condicionesIniciales=[vx, vy, inicio]
-    vship=vector(condicionesIniciales[0], condicionesIniciales[1],0)
-    ANG=pi/2
-
-    #Ángulos para trabajar en coordenadas polares
-    sa = sin(ANG)
-    ca = cos(ANG)
-    sca = sin(pimedios+ANG)
-    cca = cos(pimedios+ANG)
-
-    #Generación de objetos a representar con su posición, masa y velocidad.
-    Sun = sphere(pos=vector(0,0,0),
-                 radius=0.5, color=color.yellow,make_trail=False, interval=10)
-    Sun.mass = 1
-    Sun.v = vector(0, 0, 0)
-
-    Earth = sphere(pos=vector(1,0,0),
-                   radius=0.1, color=color.blue,make_trail=False, interval=10)
-    Earth.mass = 3.004e-6
-    Earth.v = vector(0, 6.283, 0)
-
-    Mercury = sphere(pos=vector(rmerc*ca,rmerc*sa,0),
-                     radius=0.05, color=color.orange,make_trail=False, interval=10)
-    Mercury.mass = mmerc
-    Mercury.v = vector(vmerc*cca, vmerc*sca, 0)
-
-    Venus = sphere(pos=vector(rvenus*ca,rvenus*sa,0),
-                   radius=0.1, color=color.yellow,make_trail=False, interval=10)
-    Venus.mass = mvenus
-    Venus.v = vector(vvenus*cca, vvenus*sca, 0)
-
-    Mars = sphere(pos=vector(rmarte*ca,rmarte*sa,0),
-                  radius=0.1, color=color.red,make_trail=False, interval=10)
-    Mars.mass = mmarte
-    Mars.v = vector(vmarte*cca, vmarte*sca, 0)
-
-    Jupyter = sphere(pos=vector(rjup*ca,rjup*sa,0),
-                     radius=0.3, color=color.orange,make_trail=False, interval=10)
-    Jupyter.mass = mjup
-    Jupyter.v = vector(vjup*cca, vjup*sca, 0)
-
-    Saturn = sphere(pos=vector(rsat*ca,rsat*sa,0),
-                    radius=0.25, color=color.cyan,make_trail=False, interval=10)
-    Saturn.mass = msat
-    Saturn.v = vector(vsat*cca, vsat*sca, 0)
-
-    Uranus = sphere(pos=vector(rur*ca,rur*sa,0),
-                    radius=0.2, color=color.blue,make_trail=False, interval=10)
-    Uranus.mass = mur
-    Uranus.v = vector(vur*cca, vur*sca, 0)
-
-    Neptune = sphere(pos=vector(rnep*ca,rnep*sa,0),
-                     radius=0.2, color=color.blue,make_trail=False, interval=10)
-    Neptune.mass = mnep
-    Neptune.v = vector(vnep*cca, vnep*sca, 0)
-
-    Ship = sphere(pos=vector(rship, 0, 0), radius=0.00001,
+    Ship = sphere(pos=vector(1.01, 0, 0), radius=0.00001,
                   color=color.orange,make_trail=False, interval=10)
     Ship.mass = 3.214e-28
     Ship.v = vector(vx, vy, 0.0)
-
 
     #Diferencial a usar
     dt = 2.73785078e-4
@@ -137,7 +152,7 @@ def f(vx,vy,inicio):
     distOriginal=mag(Ship.pos-Neptune.pos)-0.1
     while True:
         #Rapidez a la que se quiere visualizar cada iteración, no necesario.
-        #rate(1000000000000)
+        #rate(50)
         #Tiempo inicial del viaje de la nave, es decir, la nave puede iniciar
         #su trayectoria mucho después de que se inicie la simulación.
         if tiempo==inicio:
@@ -146,7 +161,7 @@ def f(vx,vy,inicio):
             #Efecto visual para que la trayectoria de la nave sea una linea.
             Ship.make_trail=True
         #Movemos cada uno de los cuerpos en el sistema.
-        for body in [Mercury,Venus,Earth,Mars,Jupyter,Saturn,Uranus,Neptune,Ship]:
+        for body in [Mercury,Venus,Earth,Luna,Mars,Jupyter,Saturn,Uranus,Neptune,Ship]:
             #primero la nave
             if body == Ship and tiempo>=inicio:
                 #Vectores de distancias entre los planetas, el Sol y la nave.
@@ -154,6 +169,7 @@ def f(vx,vy,inicio):
                 DM=body.pos-Mercury.pos
                 DV=body.pos-Venus.pos
                 DT=body.pos-Earth.pos
+                DL=body.pos-Luna.pos
                 DMars=body.pos-Mars.pos
                 DJ=body.pos-Jupyter.pos
                 DSat=body.pos-Saturn.pos
@@ -164,6 +180,7 @@ def f(vx,vy,inicio):
                          Mercury.mass*DM/mag(DM)**3+
                          Venus.mass*DV/mag(DV)**3+
                          Earth.mass*DT/mag(DT)**3+
+                         Luna.mass*DL/mag(DL)**3+
                          Mars.mass*DMars/mag(DMars)**3+
                          Jupyter.mass*DJ/mag(DJ)**3+
                          Saturn.mass*DSat/mag(DSat)**3+
@@ -188,13 +205,21 @@ def f(vx,vy,inicio):
             #Movimiento de cada uno de los planetas
             #Si se cambia el destino no se debe cambiar nada en este elif
             elif body!=Ship:
-                #Distancia Sol-planeta
-                distance = body.pos - Sun.pos
+                DS=body.pos-Sun.pos
+                DM=body.pos-Mercury.pos
+                DV=body.pos-Venus.pos
+                DT=body.pos-Earth.pos
+                DL=body.pos-Luna.pos
+                DMars=body.pos-Mars.pos
+                DJ=body.pos-Jupyter.pos
+                DSat=body.pos-Saturn.pos
+                DU=body.pos-Uranus.pos
+                DN=body.pos-Neptune.pos
                 #Aceleración del planeta
-                a1 = -G*Sun.mass*distance/mag(distance)**3
+                ximas1, vimas1 = acel(body.mass,DS,DM,DV,DT,DL,DMars,DJ,DSat,DU,DN)
                 #de la velocidad y posición del planeta.
-                body.pos += body.v*dt + 0.5*a1*dt*dt
-                body.v += a1*dt
+                body.pos += ximas1
+                body.v += vimas1
         #Hacemos una lista con todas las distancias Nave-Destino.
         posiciones.append(mag(Ship.pos-Neptune.pos))
         #Contador que se ocupa para saber cuando iniciar el viaje de la nave.
@@ -211,3 +236,19 @@ def f(vx,vy,inicio):
         return posiciones[0]
     print(vx,vy,inicio,mag(Ship.pos-Neptune.pos))
     return mag(ship.pos-Neptune.pos)
+
+"""Calcular aceleraciones para impedir divisiones entre cero"""
+def acel(*args):
+    global Sun, Mercury,Venus,Earth,Luna,Mars,Jupyter,Saturn,Uranus,Neptune
+    cuerpos = [Sun,Mercury,Venus,Earth,Luna,Mars,Jupyter,Saturn,Uranus,Neptune]
+    if len(args)-1==len(cuerpos):
+        acel=vector(0.0,0.0,0.0)
+        j=1
+        for i in cuerpos:
+            if mag(args[j])!=0:
+                acel += (i.mass + args[0]) * args[j] / mag(args[j]) **3
+            j+=1
+        return -G*acel
+    else:
+        raise ValueError("Los argumentos y el número de cuerpos celestes " +
+                         "deben ser iguales.")
